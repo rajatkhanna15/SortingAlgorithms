684e102ed4261992962fc26672d87a986f42953e
Rajat Khanna <rajat.khanna_cs18@gla.ac.in>
2020-07-10 13:14:40 +0530

    ArrayBubble() method starts.

--- src/BubbleSorting/ArrayBubble/ArrayBubble.java
+++ src/BubbleSorting/ArrayBubble/ArrayBubble.java
@@ -1,4 +1,14 @@
 package BubbleSorting.ArrayBubble;
 
+/**
+ * ArrayBubble class to show the implementation of Bubble sort.
+ */
 public class ArrayBubble {
+    /**
+     * Instance variables - an array to store the data items  and
+     *                    - an integer variable to store the data items.
+     */
+    private int[] intArray;
+    private int nElems;
+
 }
