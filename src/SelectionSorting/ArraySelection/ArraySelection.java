df665ebb10b6e4275e990fa09f372fa46cd05337
Rajat Khanna <rajat.khanna_cs18@gla.ac.in>
2020-07-10 21:34:04 +0530

    ArraySelection() method starts.

--- src/SelectionSorting/ArraySelection/ArraySelection.java
+++ src/SelectionSorting/ArraySelection/ArraySelection.java
@@ -1,4 +1,15 @@
 package SelectionSorting.ArraySelection;
 
+/**
+ * ArraySelection for showing the implementation of Selection Sorting.
+ */
 public class ArraySelection {
+    /**
+     * Instance variables :
+     * an array to store data items
+     * a variable to store number of elements
+     */
+    private int[] intArray;
+    private int nElems;
+
 }
