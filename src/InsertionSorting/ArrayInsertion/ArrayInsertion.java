f69f9cd3fc33d9a0bdd15588f73407e1917c1ac0
Rajat Khanna <rajat.khanna_cs18@gla.ac.in>
2020-07-11 18:55:42 +0530

    ArrayInsertion() method starts.

--- src/InsertionSorting/ArrayInsertion/ArrayInsertion.java
+++ src/InsertionSorting/ArrayInsertion/ArrayInsertion.java
@@ -1,4 +1,15 @@
 package InsertionSorting.ArrayInsertion;
 
+/**
+ * ArrayInsertion class for showing the basic implementation of Insertion Sort.
+ */
 public class ArrayInsertion {
+    /**
+     * Instance variables :
+     * intArray[] for storing the data items.
+     * nElems for storing the number of elements.
+     */
+    private int[] intArray;
+    private int nElems;
+
 }
