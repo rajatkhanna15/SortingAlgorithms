e2c14edc4cd1d48a8d97fdb0dcb450e4792a7a2e
Rajat Khanna <rajat.khanna_cs18@gla.ac.in>
2020-07-11 19:32:59 +0530

    main() method ends.

--- src/InsertionSorting/Main/Main.java
+++ src/InsertionSorting/Main/Main.java
@@ -3,6 +3,27 @@ package InsertionSorting.Main;
 /**
  * Main class which acts as a tester class for ArrayInsertion class.
  */
+import InsertionSorting.ArrayInsertion.ArrayInsertion;
+
+
+import java.util.*;
 public class Main {
 
+    public static void main(String[] args) {
+        ArrayInsertion aSelection = new ArrayInsertion(100);
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("How many items you want to enter :");
+        int n = scanner.nextInt();
+        System.out.println("Enter the data items:");
+        for (int i = 0; i < n; i++) {
+            int x = scanner.nextInt();
+            aSelection.insert(x);
+        }
+        System.out.println("The entered elements are :" );
+        aSelection.display();
+
+        System.out.println("Element in Sorted order:");
+        aSelection.insertionSort();
+        aSelection.display();
+    }
 }
